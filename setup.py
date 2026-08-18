229cdb53c661ca71af69daae226391da5d1a5bd5
HaaniPC\rak <dukson1224@gmail.com>
2026-08-18 12:23:20 +0900

    save

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.install import install
 class CustomInstallCommand(install):
   def run(self):
     # Python
-    if sys.version_info[:3] != (3, 10, 11):
+    if sys.version_info[:3] != (3, 13, 15):
       sys.exit("Only for Python 3.10.11")
 
     # OS
@@ -24,14 +24,14 @@ class CustomInstallCommand(install):
 
 setup(
   name='myserver',
-  version='0.1.4',
+  version='0.1.5',
   description='my_server_package',
   author='rhaos',
   packages=['myserver'],
   package_data={'myserver': ['*.pyd']},
   include_package_data=True,
   cmdclass={'install': CustomInstallCommand},
-  python_requires='==3.10.11',
+  python_requires='==3.13.15',
   install_requires=[
     'fastapi==0.115.8',
     'uvicorn==0.34.0',
